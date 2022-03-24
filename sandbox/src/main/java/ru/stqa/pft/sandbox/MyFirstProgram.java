50aabefc5609ac443edee0099dc8ffc46b4bb8d1
SergeyPannikov <panplex@mail.ru>
2022-03-24 17:43:40 +0300

    Сделал домашнее ДЗ 2
    Создал класс , сделал два конструктора для инициализации класса , выполнил вычисление через функцию , потом добавил вычисление через метод класса.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,10 +13,26 @@ public class MyFirstProgram {
      Rectangle r = new Rectangle(4,6);
      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-  }
+     /* Задание с 1 по 3 */
+     Point p1 = new Point(1,5);
+     Point p2 = new Point(4,9);
+     System.out.println("Расстояние между точками " + "("+ p1.x + "," + p1.y + ")" + " и " + "(" + p2.x +"," + p2.y + ")" + " будет = " + distance(p1,p2));
+
+     /* Задание 4 */
+     Point p = new Point();
+     System.out.println("Расстояние между точками " + "("+ p1.x + "," + p1.y + ")" + " и " + "(" + p2.x +"," + p2.y + ")" + " будет = " + p.distance(p1,p2));
+
+
+
+ }
 
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double distance(Point p1, Point p2) {
+     return Math.sqrt(Math.pow(p2.x- p1.x,2)+(Math.pow(p2.y-p1.y,2)));
+
     }
 
 

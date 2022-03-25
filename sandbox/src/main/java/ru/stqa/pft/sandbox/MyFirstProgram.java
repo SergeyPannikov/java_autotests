2bc7db93d2f38904fe36b1f027ea3dcd0016fe67
SergeyPannikov <panplex@mail.ru>
2022-03-25 10:37:05 +0300

    Поправил форматирование

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,7 +9,6 @@ public class MyFirstProgram {
      Square s = new Square(5);
      System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-
      Rectangle r = new Rectangle(4,6);
      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
@@ -22,18 +21,13 @@ public class MyFirstProgram {
      Point p = new Point();
      System.out.println("Расстояние между точками " + "("+ p1.x + "," + p1.y + ")" + " и " + "(" + p2.x +"," + p2.y + ")" + " будет = " + p.distance(p1,p2));
 
-
-
  }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello," + somebody + "!");
-    }
+ public static void hello(String somebody) {
+     System.out.println("Hello," + somebody + "!");
+ }
 
-    public static double distance(Point p1, Point p2) {
+ public static double distance(Point p1, Point p2) {
      return Math.sqrt(Math.pow(p2.x- p1.x,2)+(Math.pow(p2.y-p1.y,2)));
-
-    }
-
-
+ }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -14,8 +14,7 @@ public class Point {
     }
 
     public double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow(p2.x- p1.x,2)+(Math.pow(p2.y-p1.y,2)));
-
+        double sqrt = Math.sqrt(Math.pow(p2.x - p1.x, 2) + (Math.pow(p2.y - p1.y, 2)));
+        return sqrt;
     }
-
 }

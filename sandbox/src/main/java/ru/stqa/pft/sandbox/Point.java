4177e817d3ab461fb547594a0b90bdba6b4cddcd
SergeyPannikov <panplex@mail.ru>
2022-03-25 16:30:16 +0300

    Update Point.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -13,7 +13,7 @@ public class Point {
     public Point() {
     }
 
-    public double distance(Point p1, Point p2) {
+    public static double distance(Point p1, Point p2) {
         double sqrt = Math.sqrt(Math.pow(p2.x - p1.x, 2) + (Math.pow(p2.y - p1.y, 2)));
         return sqrt;
     }
